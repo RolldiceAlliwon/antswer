efd732b22441b3046962095a5a997531862cf50d
jwbro <80826587+coolkidsneversleep@users.noreply.github.com>
2026-02-09 12:28:19 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,30 +26,19 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    RecentNotesForIndex,
   ],
   left: [
     Component.PageTitle(),
     Component.SocialIcons(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
+    Component.Search(),
+    Component.Darkmode(),
     Component.Explorer(),
-    RecentNotesForIndex,
   ],
   right: [
     Component.Graph(),
@@ -66,15 +55,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.SocialIcons(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
+    Component.Darkmode(),
     Component.Explorer(),
   ],
   right: [],
