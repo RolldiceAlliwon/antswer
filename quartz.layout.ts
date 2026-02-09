20cb8dde6c29bb1b581d012fb7bb604cde05d05e
jwbro <80826587+coolkidsneversleep@users.noreply.github.com>
2026-02-09 12:34:07 +0900

    Fix import path for RecentNotesForIndex component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import RecentNotesForIndex from "./quartz/components/RecnetNotesForIndex"
+import RecentNotesForIndex from "./quartz/components/RecentNotesForIndex"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
