d38ad27019bbebad8a925dbe23ed8fd9524cc873
vega <80826587+RolldiceAlliwon@users.noreply.github.com>
2026-02-19 21:58:57 +0900

    Change page title to '🐜Antswer'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: " KJW's Digital Garden ",
+    pageTitle: " 🐜Antswer",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
